9f4a4a55422774cd231dc9c015390427a45f25d4
octopushugger <octopushugger@users.noreply.github.com>
2026-04-26 08:53:52 +0000

    Renaming with your datas

--- Supersampling/Loader.cs
+++ Supersampling/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace Supersampling
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(Supersampling));
+			gameObject.AddComponent<Supersampling>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
---
+++ Supersampling/Supersampling.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace Supersampling
+{
+	public class Supersampling : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(Supersampling)} has loaded!");
+		}
+	}
+}
